cce99a5066722807cab157046d035094d792f288
Akash Khanra <akhanra@amazon.com>
2021-05-23 21:43:59 +0530

    Initial Commit Added Dependencies

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+            <nav className="navbar navbar-dark bg-dark">
+            </nav>
+        </>
+    );
+}
